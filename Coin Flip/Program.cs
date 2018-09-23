15e8d4abc68874f605ef0660b777e21d1c26c7c4
Kristen Shea <kstar61@live.com>
2018-09-23 18:44:14 -0500

    Add project files.

---
+++ Coin Flip/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coin_Flip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
